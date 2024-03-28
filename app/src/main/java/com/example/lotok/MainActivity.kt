1395eb7f2966df78bc127d83862741b865bb4442
zaarirmoha <zaarirmo07@gmail.com>
2024-03-28 03:07:16 +0100

    first change

--- app/src/main/java/com/example/lotok/MainActivity.kt
+++ app/src/main/java/com/example/lotok/MainActivity.kt
@@ -32,7 +32,7 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
     Text(
-        text = "Hello $name!",
+        text = "zaarir $name!",
         modifier = modifier
     )
 }
