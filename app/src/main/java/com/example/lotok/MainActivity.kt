382288c385434c6633f877b4de41add76e8cd2bc
zaarirmoha <zaarirmo07@gmail.com>
2024-03-28 09:07:14 +0100

    splach Screen change1

--- app/src/main/java/com/example/lotok/MainActivity.kt
+++ app/src/main/java/com/example/lotok/MainActivity.kt
@@ -26,7 +26,7 @@ class MainActivity : ComponentActivity() {
                     color = RedBackground
                 ) {
                     LotokNavHost()
-                    //
+                    //hello
                 }
             }
         }
