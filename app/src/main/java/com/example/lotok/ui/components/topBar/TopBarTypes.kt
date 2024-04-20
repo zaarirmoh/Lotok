06bf21913425d6a753d656be83c3de426186ff93
zaarirmoha <zaarirmo07@gmail.com>
2024-04-20 20:23:24 +0100

    main

---
+++ app/src/main/java/com/example/lotok/ui/components/topBar/TopBarTypes.kt
@@ -0,0 +1,8 @@
+package com.example.lotok.ui.components.topBar
+
+import com.example.lotok.R
+
+object TopBarTypes {
+    val TopBarMenuLogNotification = R.string.TopBarMenuLogoNotification
+    val TopBarGoBackTextProfile = R.string.TopBarGoBackTextProfile
+}
