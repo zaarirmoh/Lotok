8db541f93034a6f8bd1d866cf11188403e680edc
djawadsofian <waddja4@gmail.com>
2024-04-22 02:23:18 +0100

    welcomeScreen

--- app/src/main/java/com/example/lotok/ui/screens/welcomeScreen/WelcomeScreen.kt
+++ app/src/main/java/com/example/lotok/ui/screens/welcomeScreen/WelcomeScreen.kt
@@ -6,10 +6,10 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
@@ -31,11 +31,13 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.lotok.R
 import com.example.lotok.ui.theme.LotokTheme
-//zaki
+
+
 
 @Composable
 fun WelcomeScreen(
-    modifier: Modifier = Modifier
+    modifier: Modifier = Modifier,
+    onButtonClicked: () -> Unit ,
 ) {
     val red_container_image = painterResource(id = R.drawable.red_container)
     val red_car = painterResource(id = R.drawable.red_car)
@@ -44,10 +46,10 @@ fun WelcomeScreen(
     val words = text.split(" ")
     val buttonColor = Color(android.graphics.Color.parseColor("#B3261E"))
 
-    Surface(color = Color.White, modifier = Modifier.fillMaxSize()) {
 
+    Column(modifier = modifier) {
         Box(
-            modifier = modifier.fillMaxSize(),
+            modifier = modifier,
             contentAlignment = Alignment.TopCenter
         ) {
             Image(
@@ -56,55 +58,61 @@ fun WelcomeScreen(
                 modifier = modifier
                     .fillMaxWidth()
                     .align(Alignment.TopEnd),
-                alignment = Alignment.TopEnd,
+
                 contentScale = ContentScale.FillWidth
-                //.aspectRatio(red_container_image.intrinsicSize.width / red_container_image.intrinsicSize.height)
+
             )
-            Column(modifier = modifier.fillMaxHeight()) {
-                Spacer(modifier = modifier.height(222.dp))
-                Image(
-                    painter = red_car,
-                    contentDescription = null,
-                    modifier = Modifier
-                        .size(width = 386.dp, height = 241.dp)
-                        //.aspectRatio(red_car.intrinsicSize.width / red_car.intrinsicSize.height)
+
+            Spacer(modifier = Modifier.height(55.dp))
+
+            Image(
+                painter = red_car,
+                contentDescription = null,
+                modifier = Modifier
+                    .size(width = 386.dp, height = 241.dp)
+                    .align(Alignment.BottomCenter)
+
+            )
+        }
+        Spacer(modifier = Modifier.height(55.dp))
+
+        Column(modifier=Modifier.fillMaxWidth(),horizontalAlignment = Alignment.CenterHorizontally) {
+            Text(
+                text = buildAnnotatedString {
+                    withStyle(style = SpanStyle(color = Color.Black)) {
+                        append(words.take(2).joinToString(" "))
+                    }
+                    withStyle(style = SpanStyle(color = Color.Red)) {
+                        append(" ${words.getOrNull(2) ?: ""}")
+                    }
+                },
+                fontSize = 27.sp,
+                fontWeight = FontWeight.Bold,
+            )
+
+            Text(
+                text = words.getOrNull(3) ?: "",
+                fontSize = 27.sp,
+                fontWeight = FontWeight.Bold,
+                color = Color.Black
                 )
-                Spacer(modifier = Modifier.height(55.dp))
-                Column(modifier=Modifier.fillMaxWidth(),horizontalAlignment = Alignment.CenterHorizontally) {
-                    Text(
-                        text = buildAnnotatedString {
-                            withStyle(style = SpanStyle(color = Color.Black)) {
-                                append(words.take(2).joinToString(" "))
-                            }
-                            withStyle(style = SpanStyle(color = Color.Red)) {
-                                append(" ${words.getOrNull(2) ?: ""}")
-                            }
-                        },
-                        fontSize = 27.sp,
-                        fontWeight = FontWeight.Bold,
 
-                    )
-                    Text(
-                        text = words.getOrNull(3) ?: "",
-                        fontSize = 27.sp,
-                        fontWeight = FontWeight.Bold,
-                        color = Color.Black ,
+        }
 
-                    )
 
-                }
-                Spacer(modifier = Modifier.height(56.dp))
+        Spacer(modifier = Modifier.height(56.dp))
 
-                Button(
-                    onClick = { },
-                    modifier = Modifier
-                        .align(Alignment.CenterHorizontally)
-                        .size(width = 335.dp, height = 57.dp),
-                    colors = ButtonDefaults.buttonColors( buttonColor)
-                ) {
-                    Text(button_text)
-                }
-            }
+        Button(
+            onClick = onButtonClicked,
+            modifier = Modifier
+                .fillMaxWidth()
+                .size(57.dp)
+                .padding(start = 20.dp, end = 20.dp ),
+
+
+            colors = ButtonDefaults.buttonColors(buttonColor)
+        ) {
+            Text( text = button_text, fontSize = 18.sp , color = Color.White   , fontWeight = FontWeight.Bold  )
         }
     }
 }
@@ -112,9 +120,10 @@ fun WelcomeScreen(
 
 @Preview
 @Composable
- fun WelcomeScreenPreview(){
+fun WelcomeScreenPreview(){
+    Surface(color = Color.White, modifier = Modifier.fillMaxSize()) {
     LotokTheme {
-        WelcomeScreen()
-    }
-    
+        WelcomeScreen(onButtonClicked = {})
+    }}
+
 }
