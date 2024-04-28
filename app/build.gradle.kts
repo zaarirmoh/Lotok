935d8217503ea81e538ad3b5cf13cb23bef118ba
djawadsofian <waddja4@gmail.com>
2024-04-28 07:17:50 +0100

    daya store

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -88,4 +88,7 @@ dependencies {
     implementation(libs.androidx.lifecycle.viewmodel.compose)
     implementation(libs.androidx.lifecycle.runtime.compose)
     implementation(libs.coil.compose)
+
+    implementation(libs.androidx.datastore.preferences)
+
 }
